998f801313fbed8a48e345f8b83ed10139140255
Mr.James Wong <jameswong1376@gmail.com>
2024-07-20 23:06:17 +0800

    feat: add support for MiMalloc allocator to lower usage memory.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,11 @@
 use mywebnote::cmd;
 
+// see:https://rustcc.cn/article?id=75f290cd-e8e9-4786-96dc-9a44e398c7f5
+// Check for the allocator used: 'objdump -t target/debug/mywebnote | grep mi_os_alloc'
+#[global_allocator]
+//static GLOBAL: std::alloc::System = std::alloc::System;
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
 pub fn main() {
-  cmd::execute_commands_app();
+    cmd::execute_commands_app();
 }
